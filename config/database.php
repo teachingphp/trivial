e29ece379bd4e16cf1dc0cd022671f7fe2e8c09c
marcellaceina <php.ceina.marcella@gmail.com>
2022-05-02 13:42:17 +0200

    2/5

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@ return array(
     "host"      =>"localhost",
     "user"      =>"root",
     "pass"      =>'',
-    "database"  =>"test_trivial",
+    "database"  =>"basetest",
     "charset"   =>"utf8"
 );
 ?>
