7909ac9bf8fcea66b09a5e84cde7b770eaba9a62
AlbertViSa <php.ceina.albertvilla@gmail.com>
2022-05-02 12:15:41 +0200

    Inici_PrimerDia

---
+++ view/Homepage_Inici/footer.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Inici_Peu de Pagina</title>
+        <link rel="stylesheet" type="text/css" href=".../css/Inici.css?v=<?php echo time(); ?>">
+    </head>
+    <body>
+        <footer>
+        <p>Albert Villavecchia Saez</p>
+        </footer>
+    </body>
