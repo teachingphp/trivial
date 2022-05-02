a8b3ea58d23d59a011138d66e62510998b975475
AlbertViSa <php.ceina.albertvilla@gmail.com>
2022-05-02 10:36:02 +0200

    IniciPrimerDia

---
+++ view/Homepage_Inici/Inici.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href=".../css/Inici.css?v=<?php echo time(); ?>">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <title>AlbertV</title>
+    </head>
+    <body>
+        <div id="grad1">
+            <h1>Trivial</h1>
+        </div>
+        <div class="accordion" id="accordionExample">
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    <h2>Hall of Fame</h2>
+                    </button>       
+                </h2>     
+                <div id="collapseOne" class="accordion-collapse collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingTwo">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    <h2>Instruccions</h2>
+                    </button>
+                </h2>
+                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <section>   
+                            <article>
+                                <p>Algunes pagines webs on pots veure l'Anime:</p>
+                            </article>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
