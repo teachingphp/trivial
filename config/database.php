2c9d453fb193cead9e6a5b7834da8890d10fa95c
SybelleZ <php.ceina.albertc@gmail.com>
2022-05-09 13:11:26 +0200

    Ignore Database Sybelle

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@ return array(
     "host"      =>"localhost",
     "user"      =>"root",
     "pass"      =>'',
-    "database"  =>"test",
+    "database"  =>"basadotest",
     "charset"   =>"utf8"
 );
 ?>
