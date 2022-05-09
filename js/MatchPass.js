4f0e93cbb3b0d1363923934ccf76c3abe0107a79
SybelleZ <php.ceina.albertc@gmail.com>
2022-05-09 12:28:15 +0200

    Mostrar Contra Added

--- js/MatchPass.js
+++ js/MatchPass.js
@@ -38,3 +38,25 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+const togglePassword = document.querySelector('#togglePassword');
+  const password = document.querySelector('#Contra');
+ 
+  togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
+
+const togglePassword2 = document.querySelector('#togglePassword2');
+  const password2 = document.querySelector('#ConfirmContra');
+ 
+  togglePassword2.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password2.getAttribute('type') === 'password' ? 'text' : 'password';
+    password2.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
