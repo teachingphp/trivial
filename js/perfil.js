6d2de7d8ba60be224776005ebaf3bb8fcc031d61
alberto <peacocksteam@gmail.com>
2022-05-09 12:21:11 +0200

    Cambios crear avatar

--- js/perfil.js
+++ js/perfil.js
@@ -1,19 +1,32 @@
 function cambiarAvatar(rutas){
     //alert("Avatar");
-    console.log (rutas);
+    //console.log (rutas);
 
     var rutasArr = rutas.split(';');
 
-    rutasArr.forEach(cambio);
+    console.log (rutasArr.length);
+    console.log (Math.floor( Math.random() * rutasArr.length ));
+    i = Math.floor( Math.random() * rutasArr.length ) ;
 
-    function cambio(item) {
-        if (item.length > 0){
-            var image = document.getElementById("imagen-avatar");
-            console.log (item);
-            image.src = item;
-        }
+    var image = document.getElementById("imagen-avatar");
+    console.log (rutasArr[i]);
+    image.src = rutasArr[i];
+
+    // rutasArr.forEach(cambio);
+
+    // function cambio(item) {
+    //     if (item.length > 0){
+    //         var image = document.getElementById("imagen-avatar");
+    //         console.log (item);
+    //         image.src = item;
+    //     }
        
-    }
+    // }
 
     
   }
+
+  function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+  
