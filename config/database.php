8b96e4ba673a69f5d4de8c671a58e2c0e7dc1bf5
dani3456 <php.ceina.daniel@gmail.com>
2022-05-09 13:41:17 +0200

    boton subir imagen

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@ return array(
     "host"      =>"localhost",
     "user"      =>"root",
     "pass"      =>'',
-    "database"  =>"basetest",
+    "database"  =>"test",
     "charset"   =>"utf8"
 );
 ?>
