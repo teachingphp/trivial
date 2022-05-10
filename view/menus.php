10c562c9c848726b072cfafb306eec954614d2d1
AlexPhPCeina <php.ceina.awlex@gmail.com>
2022-05-10 10:08:06 +0200

    logo ben fet

--- view/menus.php
+++ view/menus.php
@@ -77,7 +77,7 @@ if(! isset($_COOKIE["galleta"])){
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
-       <img src="../files/6296e069312cd28cf8c3fd1f8b26e13d.gif"width="60" height="60"> 
+       <img src="../files/sources/imatges/countdown.gif"width="60" height="60"> 
         
       </a> 
 
@@ -90,7 +90,7 @@ if(! isset($_COOKIE["galleta"])){
           <li><a href="#" class="nav-link px-2 link-dark">Idiomas</a></li>
         </ul>
 
-        
+
         <div class="dropdown text-end">
          <div class="text-end">
           <button type="button" onclick="login()" class="btn btn-light text-dark me-2 <?php echo $classe_amagar ?>">Login</button>
