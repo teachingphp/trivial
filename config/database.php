5218e24a5956cc1ebbd223fea20a01e36c0583ce
AlexPhPCeina <php.ceina.awlex@gmail.com>
2022-05-19 11:23:57 +0200

    cambio de base de datos pongo insert cambio href

--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
 return array(
-    "driver"    =>"mysql",
-    "host"      =>"localhost",
-    "user"      =>"root",
-    "pass"      =>'',
-    "database"  =>"test",
-    "charset"   =>"utf8"
-);
+    "driver" =>"mysql",
+    "host" =>"hl838.dinaserver.com",
+    "user" =>"wp-6e989921e9201",
+    "pass" =>'rejN6Rbb',
+    "database" =>"wp_6e989921e9201c55",
+    "charset" =>"utf8"
+    );
 ?>
